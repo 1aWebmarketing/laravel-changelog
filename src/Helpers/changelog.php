342ad1a3c948274398e98f426c8504e7c0ca1567
M. Homeister <micha@0x25.de>
2025-01-17 16:07:40 +0100

    Update changelog.php

--- src/Helpers/changelog.php
+++ src/Helpers/changelog.php
@@ -13,7 +13,7 @@ function getAppChangelog()
     return $converter->convert($markdown);
 }
 
-function getAppVersion(): string
+function getAppVersion(String $returnIfFalse = "Unknown"): string
 {
     return Cache::remember('app_version', 60, function () {
         $path = resource_path('../CHANGELOG.md');
@@ -23,6 +23,6 @@ function getAppVersion(): string
             return $matches[1];
         }
 
-        return 'Unknown';
+        return $returnIfFalse;
     });
 }
